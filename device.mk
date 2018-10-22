c4370ede2cebb42d287db052c6e3debe8737ac4d
Anton Hansson <hansson@google.com>
2018-10-22 15:33:51 +0100

    discovery: Move libnfc-nci.conf to /product
    Device-specific files belong off /system.
    
    Bug: 117878616
    Test: make
    Change-Id: Iddad62c178c553e4d628deb4509ea54451b8ab4f

--- device.mk
+++ device.mk
@@ -42,7 +42,7 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # Inherit from nile-common
